a174b926de410c6c28dbcb94df3e2db15eaac995
MartinKuschnik <Martin-Kuschnik@live.com>
2015-02-06 22:27:36 +0100

    added the pic of the debugger preview

--- WmiLight.TestApp/Program.cs
+++ WmiLight.TestApp/Program.cs
@@ -18,15 +18,14 @@ namespace WmiLight.TestApp
             Console.WriteLine("Win32_Process:");
             Console.WriteLine("------------------------");
 
-            var opt = new WmiConnectionOptions();
-            opt.EnablePackageEncryption = true;
+            var opt = new WmiConnectionOptions() { EnablePackageEncryption = true };
+            var cred = new NetworkCredential("USERNAME", "PASSWORD", "DOMAIN");
 
-            //using (WmiConnection conncetion = new WmiConnection(@"\\SERVER\root\cimv2", new NetworkCredential("Administrator", "***", "MyDomain"), opt))
-            using (WmiConnection conncetion = new WmiConnection())
+            using (WmiConnection conncetion = new WmiConnection(@"\\MACHINENAME\root\cimv2", cred, opt))
             {
-                foreach (WmiObject process in conncetion.CreateQuery("SELECT * FROM Win32_Process"))
+                foreach (WmiObject partition in conncetion.CreateQuery("SELECT * FROM Win32_DiskPartition"))
                 {
-                    Console.WriteLine(process["Name"]);
+                    Console.WriteLine(partition["Name"]);
                 }
             }
 
