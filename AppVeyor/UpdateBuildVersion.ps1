c6fb7858fb60699e8ef822a144ae4406f205ac6f
Martin Kuschnik <martin-kuschnik@LIVE.COM>
2021-12-14 17:50:05 +0100

    removed AppVoyer stuff

--- AppVeyor/UpdateBuildVersion.ps1
+++
@@ -1,7 +0,0 @@
-Write-Host "Patching Build Version to..." -NoNewline
-
-$version = $(select-string -Path "WmiLight\Properties\AssemblyInfo.cs" -Pattern "(?<=AssemblyVersion\("").*(?=\.\*""\)])" -AllMatches | % { $_.Matches } | % { $_.Value }) + $(Get-Date -format ".yy") + $(Get-Date).DayOfYear.ToString().PadLeft(3, '0') + "." + $($env:appveyor_build_number)
-
-Update-AppveyorBuild -Version $version
-
-Write-Host $version -foregroundcolor "green"
