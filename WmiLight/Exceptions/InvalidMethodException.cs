6c70afc2787e0f65f465119682909171751e53d4
Martin Kuschnik <martin-kuschnik@LIVE.COM>
2026-01-01 17:46:16 +0100

    Add specific exceptions for invalid WMI classes and methods

---
+++ WmiLight/Exceptions/InvalidMethodException.cs
@@ -0,0 +1,32 @@
+﻿using WmiLight.Wbem;
+
+namespace WmiLight
+{
+    #region Description
+    /// <summary>
+    /// The exception that is thrown when the specified WMI method is invalid.
+    /// </summary>
+    #endregion
+    internal class InvalidMethodException : WmiException
+    {
+        #region Constructors
+
+        #region Description
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvalidClassException"/> class.
+        /// </summary>
+        /// <param name="method">The method name.</param>
+        /// <param name="wbemStatus">The HRESULT.</param>
+        #endregion
+        internal InvalidMethodException(string method, WbemStatus wbemStatus)
+            : base($"The method '{method}' did not exist on the server.")
+        {
+            HResult = (int)wbemStatus;
+            Method = method;
+        }
+
+        #endregion
+
+        public string Method { get;  }
+    }
+}
